fe3456b3d04933459c1ac32abb12f6198b9284c9
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-12-05 18:45:08 +0000

    Add 'GET' and 'POST' methods to register and login routes

--- flaskmessenger/routes.py
+++ flaskmessenger/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for
+from flaskmessenger.forms import RegistrationForm, LoginForm
 from flaskmessenger import app, db
-
 
 # dummy date to iterate through using jinja for loop.
 posts = [
@@ -34,13 +34,13 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     return render_template("login.html", title='Login', form=form)
