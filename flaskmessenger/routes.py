7d9ab2848cd4252291c082a1ffcdcc7c47909665
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-12-05 11:25:18 +0000

    Add dummy data to replicate author posts

--- flaskmessenger/routes.py
+++ flaskmessenger/routes.py
@@ -2,6 +2,23 @@ from flask import render_template, url_for
 from flaskmessenger import app, db
 
 
+# dummy date to iterate through using jinja for loop.
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Message 1',
+        'content': 'First post content',
+        'date_posted': 'November 27th, 2023'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Message 2',
+        'content': 'Second post content',
+        'date_posted': 'November 27th, 2023'
+    }
+]
+
+
 @app.route("/")
 def base():
     return render_template('base.html')
@@ -9,7 +26,7 @@ def base():
 
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
