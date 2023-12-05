97a79f245b5a267842c90b9e4eba32ea718c726f
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-12-05 11:04:16 +0000

    Add routes for home and about pages, import url_for

--- flaskmessenger/routes.py
+++ flaskmessenger/routes.py
@@ -1,7 +1,17 @@
-from flask import render_template
+from flask import render_template, url_for
 from flaskmessenger import app, db
 
 
 @app.route("/")
+def base():
+    return render_template('base.html')
+
+
+@app.route("/home")
 def home():
-    return render_template("base.html")
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
