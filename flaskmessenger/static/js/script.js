97774b05614b78086a8a1d00f802b94ec3a5210b
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-12-12 21:32:30 +0000

    Add script to run the modal

--- flaskmessenger/static/js/script.js
+++ flaskmessenger/static/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // delete post modal initialization
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
 });
